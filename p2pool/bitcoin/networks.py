79efe4025a2ad7a1622ce9620c3f8a55cfa69489
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-07-27 18:06:05 -0400

    Update networks.py

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -656,7 +656,7 @@ if platform.system() == 'Darwin' else os.path.expanduser('~/.CannaCoin'), 'Canna
         BLOCK_EXPLORER_URL_PREFIX='http://explorerino.com/block/',
         ADDRESS_EXPLORER_URL_PREFIX='http://explorerino.com/address/',
         TX_EXPLORER_URL_PREFIX='http://explorerino.com/tx/',
-        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**24 - 1),
+        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**20 - 1),
         DUMB_SCRYPT_DIFF=2**16,
         DUST_THRESHOLD=0.001e8,
     ),
